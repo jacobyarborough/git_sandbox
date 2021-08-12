29164b087d32bde1e2c3f945bfb6ca038e4143a4
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-12 15:16:05 -0600

    Add global variable practice

---
+++ 50_practice/global_variables.rb
@@ -0,0 +1,161 @@
+# Global Variables: Can change and access from anywhere in the program and across classes
+
+# 1
+$global_var_1 = 10
+
+# 2
+$global_var_2 = 10.1
+
+# 3
+$global_var_3 = "Hello"
+
+# 4
+$global_var_4 = "string"
+
+# 5
+$global_var_5 = 11
+
+# 6
+$global_var_6 = "29839838nfhfns"
+
+# 7
+$global_var_7 = "157"
+
+# 8
+$global_var_8 = 157
+
+# 9
+$global_var_9 = 100
+
+# 10
+$global_var_10 = "one hundred"
+
+# 11
+$user_id = "batman11"
+
+# 12
+$used_ids = ["batman11","robin15","sidekick56"]
+
+# 13
+$password = "joker573"
+
+# 14
+$passwords = ["joker573","riddler94","gotham32"]
+
+# 15
+$text_display = false
+
+# 16
+$int_connect = true
+
+# 17
+$ip_address = "184937284040"
+
+# 18
+$time_elapsed = 32
+
+# 19
+$current_point = "level 1"
+
+# 20
+$current_page = "page 2"
+
+# 21
+$time_on_site = 115
+
+# 22
+$called = true
+
+# 23
+$running = false
+
+# 24
+$state = 4
+
+# 25
+$colors = ["red","green","blue"]
+
+# 26
+$logins = {
+  usernames: ["batman11","robin15","sidekick56"],
+  passwords: ["joker573","riddler94","gotham32"]
+}
+
+# 27
+$renewed = true
+
+# 28
+$letters = ["abc",["d","e","f"],"g","h","i"]
+
+# 29
+$attributes = {
+  strength: [42,34,56,74],
+  intelligence: [65,73,54,36],
+  stamina: [44,65,82,24]
+}
+
+# 30
+$horse_name = "blitz"
+
+# 31
+$books = ["1",2,3.0]
+
+# 32
+$hello = "world"
+
+# 33
+$access_point = 47.4
+
+# 34
+$location = "home"
+
+# 35
+$details = {
+  directions: ["left","right","north","south"],
+  names: {person1: "John", person2: "Jacob", person3: "Raplph"},
+}
+
+# 36
+$shoe_sizes = [7,8,9,10,11,12,13]
+
+# 37
+$dog_names = ["moose","bear","spot","lassy"]
+
+# 38
+$wieghts = [123,380,457]
+
+# 39
+$heights = [6.4, 5.5, 4.4]
+
+# 40
+$foo = "bar"
+
+# 41
+$bar = "foo"
+
+# 42
+$item_1 = 'Location 3'
+
+# 43
+$item_2 = "Location 1"
+
+# 44
+$brands = ['toyota',"honda","chevrolet","ford"]
+
+# 45
+$amounts = 234
+
+# 46
+$plants = ["boxwood", "sunflower", "lettuce"]
+
+# 47
+$planets = ["jupiter","saturn","mars"]
+
+# 48
+$exercises = ["push-up","sit-up","jumping-jack"]
+
+# 49
+$state_abbr = ["CO","NC","WY"]
+
+# 50
+$final = true
