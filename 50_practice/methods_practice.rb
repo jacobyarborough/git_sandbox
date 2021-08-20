6487bf6b3d99666bbf9432cfdecb119894761652
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-20 14:12:57 -0600

    Add 50_practice directory

---
+++ 50_practice/methods_practice.rb
@@ -0,0 +1,15 @@
+# 50 examples of methods
+
+# 1
+def area_circle(rad)
+  puts "The area of your circle is: #{3.14*(rad**2)}"
+end
+
+area_circle(9)
+
+# 2
+def area_rectangle(l,w)
+  puts "The area of your rectangle is: #{l*w}"
+end
+
+area_circle(43,3)
