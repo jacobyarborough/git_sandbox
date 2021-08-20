3b531b78aea792a42d0fd2621ea11e78553cbcec
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-20 09:58:52 -0600

    add class variable practice

---
+++ 50_practice/class_variables.rb
@@ -0,0 +1,41 @@
+# Class Variable Practice
+# Class variables can be accessed accross instance objects
+# Defined by @@
+
+# 1
+@@object_counter = 0
+
+# 2
+@@on_off = 1
+
+# 3
+@@class_active = false
+
+# 4
+@@class_variable_1 = 1
+
+# 5
+@@class_variable_2 = 1.0
+
+# 6
+@@class_variable_3 = 'one'
+
+# 7
+@@class_variable_4 = [1,1.0,'one']
+
+# 8
+@@class_variable_5 = {
+  first: 1,
+  second: 1.0,
+  thrid: 'one'
+}
+
+# 9
+@@class_variable_6 = {
+  "first" => 1,
+  "second" => 1.0,
+  "thrid" => "one"
+}
+
+# 10
+@@class_var_7 = true
